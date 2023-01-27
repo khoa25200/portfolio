40fdbfa66daf19ce7cab61741bbe4586cb03258b
khoa25200 <91365201+khoa25200@users.noreply.github.com>
2023-01-27 20:37:59 +0700

    update profile

--- src/api/_getProfileApi.js
+++ src/api/_getProfileApi.js
@@ -1,124 +1,121 @@
 import { FaFacebookF, FaGithub, FaLinkedinIn } from "react-icons/fa"
 // eslint-disable-next-line import/no-anonymous-default-export
-const _getProfileApi =  [
-    {
-        socialIcons: [
-            {
-                href: "https://www.linkedin.com/in/khoa2520/",
-                icon: <FaLinkedinIn />,
-            },
-            {
-                href: "https://github.com/khoa25200",
-                icon: <FaGithub />,
-            },
-            {
-                href: "https://www.facebook.com/profile.php?id=100007340288042",
-                icon: <FaFacebookF />,
-            }
+const _getProfileApi = [
+  {
+    socialIcons: [
+      {
+        href: "https://www.linkedin.com/in/khoa2520/",
+        icon: <FaLinkedinIn />,
+      },
+      {
+        href: "https://github.com/khoa25200",
+        icon: <FaGithub />,
+      },
+      {
+        href: "https://www.facebook.com/profile.php?id=100007340288042",
+        icon: <FaFacebookF />,
+      }
+    ],
+    educations: [
+      {
+        name: 'VNU HCM - UNIVERSITY OF ECONOMICS AND LAWS',
+        major: 'MANAGEMENT INFORMATION SYSTEMS',
+        subjectsRelated: 'Data Structures, Algorithms, OOPs, DBMS, Web Development, Big Data',
+        GPA: 3.05,
+        addditionInfor: [],
+        times: 'July 2019 - Present'
+      }
+    ],
+    experiences: [
+      {
+        _id: '1',
+        roleName: 'Software Engineer - Intern (Frontend, ReactJS)',
+        companyName: 'Kyanon Digital - Digital Transformation & Agile Engineering',
+        companyUrl: 'https://kyanon.digital/',
+        skills: 'ReactJS, NextJS, JavaScript, Typescript, Redux, SCSS, Chakra - UI, Git',
+        responsibilities: [
+          'Responsible to handle the Frontend: responsive, UI, ...',
+          'Implement new features/fix bug to the web',
+          'Maintenance of existing projects launched',
+          'Implement many alternative solution for a problem.'
         ],
-        educations: [
-            {
-                name: 'VNUHCM-UNIVERSITY OF ECONOMICS ANF LAWS',
-                major:'MANAGEMENT INFORMATION SYSTEMS',
-                subjectsRelated:'Data Structures, Algorithms, OOPs, OS, DBMS, Web Development, Big Data',
-                GPA: 3.05,
-                addditionInfor: [],
-                times: 'July 2019 - Present'
-            }
+        times: 'Oct 2022 - Feb 2023'
+      },
+      {
+        _id: '2',
+        roleName: 'Web Designer - FullTime (Wordpress)',
+        companyName: 'BMS Global - Agency Company',
+        companyUrl: 'https://bmsglobal.vn/',
+        skills: 'HTML, CSS, JavaScript, PHP, MySQL, Wordpress',
+        responsibilities: [
+          'Build website for small and medium business',
+          'Implement new features to the ecommerce website using WORDPRESS',
+          'Design layout and animation for website',
+          'Optimize website in terms of interface (mobile & tablet) and page load speed (90+ google page speed).',
+          'Custom new website on the existing template'
         ],
-        experiences: [
-            {   
-                _id: 'abc',
-                roleName: 'experience',
-                skills:'',
-                responsibilities: [
-                    'Responsible to handle the Frontend as well as Backend.',
-                    'Implement new features to the apps.',
-                    'Make the app very efficient.',
-                    'Implement many alternative solution for a problem.'
-                ],
-                times: 'June 2021 - Present'
-            }
+        times: 'Jun 2022 - Sep 2022'
+      },
+      {
+        _id: '3',
+        roleName: 'Designer - PartTime',
+        companyUrl: '',
+        companyName: 'Az-Media Designer - PartTime',
+        skills: 'Photoshop, AI, Adobe XD, Figma',
+        responsibilities: [
+          'Design banners, publications, and images for posts on the fan page.',
+          'Building brand identities: logos, employee cards, clothes, etc. for corporate customers'
         ],
-        skills: {
-            skills: [
-              {
-                title: "C / C++",
-                progress: "75%",
-              },
-              {
-                title: "Java",
-                progress: "75%",
-              },
-              {
-                title: "Javascript",
-                progress: "65%",
-              },
-              {
-                title: "Python",
-                progress: "45%",
-              },
-              {
-                title: "HTML",
-                progress: "90%",
-              },
-              {
-                title: "CSS",
-                progress: "80%",
-              },
-              {
-                title: "Dart",
-                progress: "65%",
-              },
-            ],
-            frameworks: [
-              {
-                title: "React.js",
-                progress: "90%",
-              },
-              {
-                title: "Flutter",
-                progress: "85%",
-              },
-              {
-                title: ".NET",
-                progress: "70%",
-              },
-              {
-                title: "Angular.js",
-                progress: "65%",
-              },
-              {
-                title: "Vue.js",
-                progress: "60%",
-              },
-              {
-                title: "Redux",
-                progress: "80%",
-              },
-              {
-                title: "Framer Motion",
-                progress: "40%",
-              },
-              {
-                title: "Photoshop / Figma / UI / UX",
-                progress: "60%",
-              },
-              {
-                title: "Django",
-                progress: "50%",
-              },
-              {
-                title: "Bootstrap",
-                progress: "70%",
-              },
-              {
-                title: "Material-UI",
-                progress: "80%",
-              },
-            ],
-          }
-
+        times: 'Feb 2022 - Jun 2022'
+      }
+    ],
+    skills: {
+      skills: [
+        {
+          title: "Javascript",
+          progress: "90%",
+        },
+        {
+          title: "Typescript",
+          progress: "70%",
+        },
+        {
+          title: "NodeJS",
+          progress: "60%",
+        },
+        {
+          title: "HTML, CSS",
+          progress: "95%",
+        },
+        {
+          title: "Python",
+          progress: "45%",
+        },
+      ],
+      frameworks: [
+        {
+          title: "ReactJS",
+          progress: "90%",
+        },
+        {
+          title: "Angular",
+          progress: "65%",
+        },
+        {
+          title: "Photoshop / Figma / UI / UX",
+          progress: "60%",
+        },
+        {
+          title: "Bootstrap",
+          progress: "70%",
+        },
+        {
+          title: "Material-UI",
+          progress: "80%",
+        },
+      ],
     }
+
+  }
 ]
 export default _getProfileApi
