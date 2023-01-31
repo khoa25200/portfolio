3feeadf91e60a30c8f596c7937e3ba5a12f5bd60
khoa25200 <91365201+khoa25200@users.noreply.github.com>
2023-01-31 15:56:42 +0700

    update foody project nextjs

--- src/api/project.js
+++ src/api/project.js
@@ -1,12 +1,20 @@
 import lunar from "../assets/portfolio/lunarnewyear.png"
+import foody from "../assets/portfolio/foodyapp.png"
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
   {
     img: lunar,
     title: "LUNAR NEW YEAR 2023",
-    desc: "Lunar new year 2023",
+    desc: "Using Html, Css, Javascript, React",
     demo: "https://lunarnewyear2023.vercel.app/",
     source: "https://github.com/khoa25200/lunarnewyear2023",
-  }
+  },
+  {
+    img: foody,
+    title: "FOODY APP",
+    desc: "Using Html, Css, Typescript, NextJS, Redux",
+    demo: "https://foody-app-delta.vercel.app/",
+    source: "https://github.com/khoa25200/foody-app",
+  },
 ]
