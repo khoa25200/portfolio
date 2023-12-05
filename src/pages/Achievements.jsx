e4665d69bedef9fc63fc869b4d9ed062ff716dbc
khoa25200 <91365201+khoa25200@users.noreply.github.com>
2023-12-05 11:42:47 +0700

    update some info

--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -20,7 +20,7 @@ export default function Achievements() {
                   <BsTrophy />
                 </i>
               </span>
-              <a onClick={() => window.open(LR)}>TOEIC Reading & Listening 👉 560/990</a>
+              <a onClick={() => window.open(LR)}>TOEIC Reading & Listening 👉 545/990</a>
             </div>
             <div className="pointer">
               <span className="fa-li p-2">
@@ -29,6 +29,43 @@ export default function Achievements() {
                 </i>
               </span>
               <a onClick={() => window.open(SW)}>TOEIC Speaking & Writing 👉 260/400</a>
+            </div>
+          </li>
+          <br />
+          <br />
+          <li> 
+            <h4>Hacker Rank Certification:</h4>
+            <div className="pointer">
+              <span className="fa-li p-2">
+                <i className="fas fa-trophy text-warning">
+                  <BsTrophy />
+                </i>
+              </span>
+              <a onClick={() => window.open("https://www.hackerrank.com/certificates/4d19bdef9edf")}>JavaScript (Intermediate) Certificate</a>
+            </div>
+            <div className="pointer">
+              <span className="fa-li p-2">
+                <i className="fas fa-trophy text-warning">
+                  <BsTrophy />
+                </i>
+              </span>
+              <a onClick={() => window.open("https://www.hackerrank.com/certificates/2aa09968e62c")}>JavaScript (Basic) Certificate</a>
+            </div>
+            <div className="pointer">
+              <span className="fa-li p-2">
+                <i className="fas fa-trophy text-warning">
+                  <BsTrophy />
+                </i>
+              </span>
+              <a onClick={() => window.open("https://www.hackerrank.com/certificates/92063b378d7b")}>Node (Basic) Certificate</a>
+            </div>
+            <div className="pointer">
+              <span className="fa-li p-2">
+                <i className="fas fa-trophy text-warning">
+                  <BsTrophy />
+                </i>
+              </span>
+              <a onClick={() => window.open("https://www.hackerrank.com/certificates/742903a96f27")}>Software Engineer Certificate</a>
             </div>
           </li>
         </ul>
